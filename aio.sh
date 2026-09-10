3860754824fd7d25d20aad960bfbb7e9d60ce56e
guruzgh-ed <eddyunicof@gmail.com>
2026-09-10 13:23:26 +0000

    Update aio.sh

--- aio.sh
+++ aio.sh
@@ -1,10 +1,1 @@
-# Multi Script
-<br>
 
-#### Installation
-```
-bash <(curl -sL https://raw.githubusercontent.com/guruzgh-ed/guruz/master/aio.sh)
-```
-           
-----
-Bash script by Eddme23 & Guruz GH
